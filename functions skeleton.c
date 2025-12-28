3cd98a9ab9e0f0183b12f86a76102b7accea50ed
chorangesteven-afk <chorangesteven@gmail.com>
2025-12-28 15:09:07 -0500

    Create functions skeleton.c

---
+++ functions skeleton.c
@@ -0,0 +1,22 @@
+/*
+  This program is used to help students excercise creating user defined functions.
+
+  By: Larry Snedden
+*/
+
+//Pre processor directives go here
+#include <stdio.h>
+
+//Function declarations go in this area
+ 
+int main(void)
+{
+  //Local declarations go here such as variables
+  
+  //Local statements go here. In this case function calls
+  
+  //below is the return statement which ends the programreturn 0;
+
+}//end main function
+ 
+//This area is for all other user-defined functions and their definitions.
